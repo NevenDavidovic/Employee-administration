2b1ded1564b8c76b6d7318dfa5eb521e114c595b
Neven Davidovic <neven4380@live.com>
2022-08-02 22:54:10 +0200

    Komit drugi

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,jsonify, request
+from flask import Flask, render_template
 
-app= Flask(__name__)
+app=Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World"
+def Index():
+    return render_template("index.html")
 
-if __name__== "__main__":
+
+if __name__=="__main__":
     app.run(debug=True)
